6c64b131f0757eb022d13fc98ad383d2c656ead9
U Sen Wan <senwan96@gmail.com>
2022-02-21 18:03:17 +0600

    happy coding

---
+++ prime.py
@@ -0,0 +1,8 @@
+def isPrime(n):
+    for i in range(2,n+1):
+        if(n % i != 0):
+            return True
+        else:
+            return False
+if __name__=="__main__":
+    print(isPrime(13))
